40eec9b55e12c96602acc7da91259ef1c58d6de0
Gerald Büchler <dev@genomast.de>
2023-05-24 22:12:47 +0200

    switch from omxplayer 2 mpg321
    omxplayer is no longer available in the repo.
    So i decided to switch to another program.
    
    mpg321 was an recommendation i found
    it worked fine.

--- autostartPlayer.sh
+++ autostartPlayer.sh
@@ -8,22 +8,20 @@ do
 	echo "Wait for a key"
 	read aok
 	echo ${aok}
-	if [ "${aok}" == "0580751364" ]; then
+	if [ "${aok}" == "$(more /daten/parentkey.txt)" ]; then
 		if [ "${pmode}" == "1" ]; then
 			pmode=0
 			echo "Parentmode is set to OFF"
 		else
 			pmode=1
 			echo "Parentmode is set to ON"
-			killall runplaylist.sh
-			killall omxplayer.bin
+			killall mpg321
 		fi
 	else
 		if [ "${pmode}" == "0" ]; then
 			echo "Parentmode OFF"
-			killall runplaylist.sh
-			killall omxplayer.bin
-			/usr/local/bin/runplaylist.sh ${aok} &
+			killall mpg321
+			mpg321 /daten/${aok}/* &
 		else
 			echo "Parentmode ON"
 		fi
